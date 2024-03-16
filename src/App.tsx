0f4603a4e808840b31eda94f7e75185dc8bc80b9
jonPlumb5789 <jonplumb89@outlook.com>
2024-03-16 08:53:04 -0400

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { AudioManager } from "./components/AudioManager";
+import Transcriber from "./components/NoteTaker/Tanscriber";
 import Transcript from "./components/Transcript";
 import { useTranscriber } from "./hooks/useTranscriber";
 
@@ -9,23 +10,13 @@ function App() {
         <div className='flex justify-center items-center min-h-screen'>
             <div className='container flex flex-col justify-center items-center'>
                 <h1 className='text-5xl font-extrabold tracking-tight text-slate-900 sm:text-7xl text-center'>
-                    Whisper Web
+                    Relax and take notes...
                 </h1>
                 <h2 className='mt-3 mb-5 px-4 text-center text-1xl font-semibold tracking-tight text-slate-900 sm:text-2xl'>
-                    ML-powered speech recognition directly in your browser
+                    - digital conference room transcriber -
                 </h2>
                 <AudioManager transcriber={transcriber} />
                 <Transcript transcribedData={transcriber.output} />
-            </div>
-
-            <div className='absolute bottom-4'>
-                Made with{" "}
-                <a
-                    className='underline'
-                    href='https://github.com/xenova/transformers.js'
-                >
-                    🤗 Transformers.js
-                </a>
             </div>
         </div>
     );
