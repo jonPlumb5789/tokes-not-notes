db0f112fc7ee7644acf65741685bdbac9fc2b700
jonPlumb5789 <jonplumb89@outlook.com>
2024-03-16 10:50:10 -0400

    grahhffh

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/whisper-web/",
+  base: "/tokes-not-notes/",
   plugins: [
     
     react()
